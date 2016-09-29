a93b202839458a314d9b8f83c96446567407f90d
Michael Finneran <mrfinneran@gmail.com>
2016-09-29 13:39:07 -0400

    update day 2 slides

---
+++ 2-data-types/starter_code/mad_libs/array_practice/array.js
@@ -0,0 +1,43 @@
+/**
+ * Arrays
+ * Most of your answers should be stored in variables called q1, q2 etc..
+ * and the variables printed to the console.
+ * (i.e) console.log("Question 1" + q1)
+ */
+
+/**
+ * Question 1
+ * Create an array of image source filenames.
+ * Use "image1.png", "image2.png", and "image3.png" as the array values.
+ */
+
+// Your code here
+
+/**
+ * Question 2
+ * Using the array from Question 1, store the first element of the array
+ * in variable q2.
+ */
+
+// Your code here
+
+/**
+ * Question 3
+ * Get the length of the first array (number of elements in the array)
+ * and store it in variable q3
+ */
+
+// Your code here
+
+/**
+ * Question 4
+ * Using the array from Question 1, store the last element of the array
+ * in variable q4. Hint: How can we get the number of elements in the array?
+ */
+
+
+// Your code here
+
+
+// ____________________________________________________________________________
+
